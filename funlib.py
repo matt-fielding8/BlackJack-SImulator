5b5bfaceff8033d9882f60ff4d5b8ec373f6e4fe
Matthew Fielding <mattfielding8@gmail.com>
2019-05-08 11:03:54 +0100

    Revert "Update: card_decks function to take input"
    This reverts commit 55f1a6a39a6a31f9a7231415381afaccf8145d5d.

--- funlib.py
+++ funlib.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 import random as rd
 
-def card_decks():
-    '''' () -> list of lists
+def card_decks(n):
+    '''' (int) -> list of lists
 
-    Creates n number of card decks arrays consisting of 4 sets, with card id at index 0 and card value at index [1:].
+    Creates n number of card decks arrays consisting of 4 suited sets, with card id at index 0 and card value at index [1:].
 
-    >>> card_decks():
+    >>> card_decks(2):
     [[[['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]],
     [['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]],
     [['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]],
@@ -15,19 +15,19 @@ def card_decks():
     '''
 
     playing_deck = []
-    n = input("How many decks of cards would you like? ")
     card_array = [['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
                    [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]]
 
     if n != 0:
-        for i in range(int(n)*4):
+        for i in range(n*4):
             playing_deck.append(card_array)
     else:
         return "Zero is not a valid number of card decks"
 
+
     return playing_deck
 
-# print(card_decks())
+# print(card_decks(1))
 
 def player_info():
     ''' () -> dict {player: score}
