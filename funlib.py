29d6cf2551dc1e71d808bd5434ab4743e765b404
Matthew Fielding <mattfielding8@gmail.com>
2019-07-05 17:34:10 +0100

    Refactor: update player_info to include dealer
    Dealer included as a key index in the dictionary. Starting values of
    the player hands changed from 0 to empty string, which makes more sense
    in the initial dictonary output.

--- funlib.py
+++ funlib.py
@@ -7,7 +7,7 @@ def card_decks(n):
 
     Creates n number of card decks arrays consisting of 4 suited sets, with card id at index 0 and card value at index [1:].
 
-    >>> card_decks(2):
+    >>> card_decks(1):
     [[[['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]],
     [['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]],
     [['A', 1, 11], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['J', 10], ['Q', 10], ['K', 10]],
@@ -43,9 +43,11 @@ def player_info():
             print('Player {player} is already playing.'.format(player = player))
 
         if player not in player_dct:
-            player_dct[player] = 0
+            player_dct[player] = ''
 
         player = input("Enter player name (or 'p' to play): ")
+
+    player_dct['Dealer'] = ''
 
     return player_dct
 
@@ -96,6 +98,19 @@ def select_card_id(card):
     card_id = card[0]
 
     return card_id
+
+def deal_cards(players, deck):
+    ''' (dict, list of list) -> list of list [str, str, int]
+
+    Deals 2 random cards from deck for each player in players. Returns each player hand as a string,
+    dealer hand showing 1 card only.
+
+    >>> deal_cards(players, deck):
+    [['ding:', 'A J', 20], ['dong:', '2 3', 5], ['Dealer:', 'K', 10]]
+    '''
+    
+
+
 
 
 
